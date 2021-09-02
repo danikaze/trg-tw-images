0bf6894ef0aadc55a54ac1b8ff6bd50086ec0a93
danikaze <danikaze@gmail.com>
2021-09-02 21:35:24 +0900

    TS + webpack build

---
+++ build-time-constants/constants.d.ts
@@ -0,0 +1,11 @@
+/*
+ * Data from constants.js
+ */
+/** Example of a value available in the whole application */
+declare const EXAMPLE: string;
+
+/*
+ * Data from secrets.js
+ */
+/** Example of a secret value available in the whole application */
+declare const SECRET_EXAMPLE: string;
